2479d14bd1b3d612de4645c00fa838b6236860bd
alberto <alyoung1991@gmail.com>
2021-11-16 17:02:10 -0500

    Resolved conflicts in app.js

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -5,13 +5,10 @@ import NavBarContainer from './nav/navbar_container';
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
-<<<<<<< HEAD
 import ProfileContainer from './profile/profile_container';
 import QuestionsFormContainer from './questions_form/questions_form_container';
-=======
 import UserProfileContainer from './profile/user_profile_container';
 import HomeProfileContainer from './profile/home_profile_container';
->>>>>>> 55dbfc07de6e80168be7db22229fea3ae2535bbf
 import '../stylesheets/base.scss';
 
 const App = () => {
@@ -26,14 +23,11 @@ const App = () => {
           <AuthRoute exact path="/" component={MainPage} />
           <AuthRoute exact path="/login" component={LoginFormContainer} />
           <AuthRoute exact path="/signup" component={SignupFormContainer} />
-<<<<<<< HEAD
           <ProtectedRoute path="/users/:username" component={ProfileContainer} />
           <ProtectedRoute exact path="/profile" component={ProfileContainer} />
           <ProtectedRoute exact path="/questions" component={QuestionsFormContainer} />
-=======
           <ProtectedRoute path="/users/:username" component={UserProfileContainer} />
           <ProtectedRoute exact path="/profile" component={HomeProfileContainer} />
->>>>>>> 55dbfc07de6e80168be7db22229fea3ae2535bbf
       </Switch>
     </div>
   );
