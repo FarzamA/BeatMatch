89598ddfecedb55edc9044975caf649000443a03
Farzam Ahmad <farzam135@gmail.com>
2021-11-16 11:32:09 -0500

    add get all questions route

--- routes/api/questions.js
+++ routes/api/questions.js
@@ -4,12 +4,23 @@ const mongoose = require('mongoose');
 
 const Question = require('../../models/Question');
 
+
+// Get a single question
 router.get('/:id', (req, res) => {
     Question.findById(req.params.id)
         .then(question => res.json(question))
         .catch(err => 
             res.status(404).json({ noQuestionFound: 'No question found'}));
 });
+
+// Get all questions
+router.get('/', (req, res) => {
+    Question.find()
+        .then(questions => res.json(questions))
+        .catch(err => 
+            res.status(404).json({ noQuestionsFound: 'No questions found'}));
+});
+
 
 // allow for users to submit their own questions in the future?
 router.post('/', (req, res) => {
