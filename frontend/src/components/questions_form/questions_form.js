400828cab3e4ef217df3b78cad157ec562481c34
alberto <alyoung1991@gmail.com>
2021-11-16 15:51:41 -0500

    Added question answers to form

--- frontend/src/components/questions_form/questions_form.js
+++ frontend/src/components/questions_form/questions_form.js
@@ -11,17 +11,38 @@ class QuestionsForm extends React.Component {
     componentDidMount(){
         this.props.fetchQuestions();
     }
+
+    handleSubmit(){
+        e.preventDefault();
+
+        console.log()
+    }
     
     render(){
+        console.log(this.props.questions)
         if(!this.props.questions){
             return null;
         }
         return (
-            <div>
+            <form onSubmit={this.handleSubmit}>
                 {this.props.questions.map((question, i) => {
-                    return <div key={i}>{question.question}</div>;
+                    return (
+                        <div key={`question-${i}`}>
+                            <label>{question.question}<br/>
+                                {question.answerOptions.map((answerOption, j) => {
+                                    return (
+                                        <label key={`answer-${j}`}>{answerOption}
+                                            <input type="radio" value={answerOption} />
+                                        </label>
+                                    );
+                                })}
+                            </label>
+                        </div>
+                        
+                    );
                 })}
-            </div>
+                <input type="submit" value="Create Playlist"/>
+            </form>
         );
     }
 }
