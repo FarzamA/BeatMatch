5fca1548187cc4b76a9cf13bc317f821d4f8e85d
Farzam Ahmad <farzam135@gmail.com>
2021-11-16 02:44:45 -0500

    working on populating question schema

---
+++ models/Question.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const QuestionSchema = new Schema({
+    question: {
+        type: String,
+        required: true
+    },
+    targetCategory: {
+        type: String, 
+        required: true
+    }
+})
