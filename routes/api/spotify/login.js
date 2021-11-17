0a4ddb08ff33860e97804f879904341e881192c2
Zeki <zckural@gmail.com>
2021-11-17 02:56:11 -0500

    spotify login and get profile

--- routes/api/spotify/login.js
+++ routes/api/spotify/login.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const axios = require("axios");
-const querystring = require("querystring")
+const queryString = require("query-string");
 const router = express.Router();
-const { clientId, clientSecret } = require("../../../config/keys");
+const { clientId, clientSecret, redirectUri } = require("../../../config/keys");
+
+const generateRandomString = (length) => {
+  var text = "";
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
 
 const getToken = async () => {
   const response = await axios({
@@ -21,8 +32,78 @@ const getToken = async () => {
     },
   });
   const token = response.data.access_token;
-  console.log(token)
+  console.log(token);
   return token;
 };
+
+router.get("/", async (req, res) => {
+  let state = generateRandomString(16);
+  const scope = "user-read-private user-read-email";
+  const params = {
+    client_id: clientId,
+    response_type: "code",
+    redirect_uri: redirectUri,
+    state,
+    scope,
+  };
+  const url =
+    "https://accounts.spotify.com/authorize?" + queryString.stringify(params);
+  console.log(url);
+  console.log("in login");
+  res.redirect(url);
+});
+
+const getAccessToken = async (code) => {
+  const tokenResponse = await axios({
+    url: "https://accounts.spotify.com/api/token",
+    method: "post",
+    params: {
+      code: code,
+      redirect_uri: redirectUri,
+      grant_type: "authorization_code",
+    },
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    auth: {
+      username: clientId,
+      password: clientSecret,
+    },
+    json: true,
+  });
+  const token = tokenResponse.data.access_token;
+  console.log(token);
+  return token;
+};
+
+const getProfile = async (token) => {
+    const response = await axios ({
+        url: "https://api.spotify.com/v1/me",
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+    return response.data
+}
+router.get("/callback", async (req, res) => {
+  const code = req.query.code || null;
+  //   const storedState = state;
+  let state = req.query.state || null;
+  //   console.log(storedState);
+  console.log(req.cookies)
+  console.log(state);
+  console.log("in callback");
+
+  try {
+    const spotifyToken = await getAccessToken(code);
+    const myprofile = await getProfile(spotifyToken)
+    res.send({ myprofile });
+  } catch (err) {
+    console.log(err);
+    res.statusMessage = "Could not get songs";
+    res.status(400).end();
+  }
+});
 
 module.exports = router;
