0714aabcdde9eb66e952caedcbb670ade04d35f2
alberto <alyoung1991@gmail.com>
2021-11-17 16:34:03 -0500

    Added questions and main page styling

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 class MainPage extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.checkScrollPosition = this.checkScrollPosition.bind(this);
+  }
+
+  checkScrollPosition(){
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      this.props.history.push({
+          pathname: '/questions'
+      });
+    }
+  }
+
+  componentDidMount(){
+    document.addEventListener('scroll', this.checkScrollPosition);
+  }
+
+  componentWillUnmount(){
+    document.removeEventListener('scroll', this.checkScrollPosition);
+  }
+
   render() {
     return (
-      <div>
-        <h4>This is the main page</h4>
+      <div className="main-page">
+        <div>This is the main page</div>
       </div>
     );
   }
 }
 
-export default MainPage;
+export default withRouter(MainPage);
