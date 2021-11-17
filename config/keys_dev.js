0e00a99faaedb544571f5bb7d8c388016b422afc
Elliot Wilson <87868033+elliot-wilson@users.noreply.github.com>
2021-11-17 14:01:56 -0500

    remove secr

--- config/keys_dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://elliot:hhDT4DAmw5hgyWIc@cluster0.phwxi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    secretOrKey: 'sk$o&+#z-<SSih.'
-}
