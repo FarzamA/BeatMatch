26bddecd9489b693d960861a219a22fc60e1c80f
Farzam Ahmad <farzam135@gmail.com>
2021-11-17 04:43:31 -0500

    added validation check for users that are already followed

--- routes/api/follows.js
+++ routes/api/follows.js
@@ -6,26 +6,30 @@ const Follow = require('../../models/Follow');
 const User = require('../../models/User');
 
 // Get all users that the current/any user is following
-router.get('/user/:user_id', (req, res) => {
+router.get('/following/:user_id', (req, res) => {
     Follow.find({ follower_id: req.params.user_id })
         .then(following => {
-            const followedUsers = {}
+            let followedUsers = {};
             following.forEach(follow => {
                 User.findOne({ _id: follow.following_id })
-                    .then(user => followedUsers[user._id] = user)
+                    .then(user => {     
+                        // console.log(user);
+                        followedUsers[user._id] = user;
+                        // console.log(followedUsers, 'followedUsers');
+                    })
+                    res.json({followedUsers});
             });
-
-            res.json(followedUsers);
+            console.log(followedUsers);
         })
         .catch(err => 
             res.status(404).json({ noFollowingFound: 'This user does not follow anyone' }))
 });
 
 // Get all users that follow current/specific user 
-router.get('/user/:user_id', (req, res) => {
+router.get('/followers/:user_id', (req, res) => {
     Follow.find({ following_id: req.params.user_id })
         .then(followers => {
-            const followerUsers = {}
+            let followerUsers = {}
             followers.forEach(follow => {
                 User.findById(follow.followers_id)
                     .then(user => followerUsers[user._id] = user)
@@ -39,71 +43,71 @@ router.get('/user/:user_id', (req, res) => {
 
 // Create a follow and increment following count for req.body user(current user)
 router.post('/:user_id', (req, res) =>{
-    const newFollow = new Follow({
-        follower_id: req.body.user_id,
-        following_id: req.params.user_id
-    });
-    let resFollow;
-    newFollow.save().then(follow => resFollow = follow).catch(err => console.log(err));
-
-    // Increment following count of current user
-    User.findById(req.body.user_id)
-        .then(user => {
-            user.following += 1;
-            const currentUser = user;
-            user.save();
-            // res.json({ currentUserFollowingCount: user.following });
-
-            // Increment follow count of followed user 
-            User.findById(req.params.user_id)
-            .then(user => {
-                user.followers += 1;
-                user.save();
-                res.json({
-                    followedUserFollowerCount: user.followers,
-                    currentUserFollowingCount: currentUser.following,
-                    follow: resFollow
+    Follow.findOne({ follower_id: req.body.user_id, following_id: req.params.user_id})
+        .then(follow => res.status(400).json({ alreadyFollow: 'You already follow this user' }))
+        .catch(err => {
+            const newFollow = new Follow({
+                follower_id: req.body.user_id,
+                following_id: req.params.user_id
+            });
+            let resFollow;
+            newFollow.save().then(follow => resFollow = follow).catch(err => console.log(err));
+        
+            // Increment following count of current user
+            User.findById(req.body.user_id)
+                .then(user => {
+                    user.following += 1;
+                    const currentUser = user;
+                    user.save();
+                    // res.json({ currentUserFollowingCount: user.following });
+        
+                    // Increment follow count of followed user 
+                    User.findById(req.params.user_id)
+                    .then(user => {
+                        user.followers += 1;
+                        user.save();
+                        res.json({
+                            followedUserFollowerCount: user.followers,
+                            currentUserFollowingCount: currentUser.following,
+                            follow: resFollow
+                        })
+                    })
+                    .catch(err => console.log(err));
                 })
-            })
-            .catch(err => console.log(err));
+                .catch(err => console.log(err));
         })
-        .catch(err => console.log(err));
-
-    
-
 });
 
 // Remove a follow
 router.delete('/:id', (req, res) => { 
-    Follow.deleteOne({ _id: req.params.id }, function(err, follow) {
+    // let currentUser;
+    // let followedUser;
+
+    Follow.findById(req.params.id).then(follow => {
         // Decrement following count of current user
         User.findById(follow.follower_id)
-            .then(user => {
-                user.following -= 1;
-                const currentUser = user;
-                user.save();
-                // res.json({ currentUserFollowingCount: user.following });
+        .then(user => {
+            user.following -= 1;
+            // currentUser = user.following;
+            user.save();
+            // res.json({ currentUserFollowingCount: user.following });
 
-                
-                // Decrement follow count of followed user 
-                User.findById(follow.following_id)
-                .then(user => {
-                    user.followers -= 1;
-                    user.save();
-                    res.json({ 
-                        followedUserFollowerCont: user.followers,
-                        currentUserFollowingCount: currentUser.following,
-                        follow: follow
-                    });
-                })
-                .catch(err => console.log(err));
+            
+            // Decrement follow count of followed user 
+            User.findById(follow.following_id)
+            .then(user => {
+                user.followers -= 1;
+                // followedUser = user.followers;
+                user.save();
             })
             .catch(err => console.log(err));
-    
+        })
+        .catch(err => console.log(err));
+    }).catch(err => console.log(err));
 
-        // res.json(follow);
-    })
-    .catch(err => console.log(err));
+    Follow.deleteOne({ _id: req.params.id }).then(follow => res.json({ 
+        follow: follow
+    })).catch(err => console.log(err));
 
 
 });
