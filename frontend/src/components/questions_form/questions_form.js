7790145e2abaf536a29bff58933bb384b906dc9a
alberto <alyoung1991@gmail.com>
2021-11-18 00:05:49 -0500

    Refactored logic to question form and added styling to main page and question form

--- frontend/src/components/questions_form/questions_form.js
+++ frontend/src/components/questions_form/questions_form.js
@@ -5,7 +5,10 @@ class QuestionsForm extends React.Component {
         super(props);
         this.state = {
             pageNum: 1,
-            prevAnswer: 0,
+            prevBgColor: '#131316',
+            prevTextColor: 'white',
+            bgColors: ['#ff60b8', '#1a71eb', '#fffa69', '#ed3747', '#ed3747', '#b81d45', '#b81d45', '#d6dd21', '#7dcdef', '#7dcdef', '#7dcdef', '#4cc141'],
+            textColors: ['#b81d45', '#ff6783', '#ff6783', '#ff60b8', '#4cc141', '#d6dd21', '#672d91', '#0d4215', '#fffa69', '#ed3747', '#1a71eb', '#531754'],
             questions: this.props.questions,
             answers: new Array(this.props.questions.length),
             targetCategories: this.props.targetCategories
@@ -13,6 +16,8 @@ class QuestionsForm extends React.Component {
 
         this.handleInput = this.handleInput.bind(this);
         this.generatePlaylist = this.generatePlaylist.bind(this);
+        this.handleMouseEnter = this.handleMouseEnter.bind(this);
+        this.handleMouseLeave = this.handleMouseLeave.bind(this);
     }
 
     componentDidMount(){
@@ -22,43 +27,120 @@ class QuestionsForm extends React.Component {
 
     componentWillUnmount(){
         const body = document.querySelector('body')
-        body.classList.remove('first-negative-answer');
-        body.classList.remove('first-positive-answer');
-        body.classList.remove('positive-answer');
-        body.classList.remove('negative-answer');
+        body.classList.remove('first-answer');
+        body.classList.remove('following-answer-1');
+        body.classList.remove('following-answer-2');
+        body.style.backgroundColor = '#131316';
     }
 
     handleInput(answerIndex, answerValue){
         const body = document.querySelector('body');
-        if(this.state.pageNum === 1){
-            if(answerValue === 0){
-                body.classList.add('first-negative-answer');
-            }else {
-                body.classList.add('first-positive-answer');
-            }
+        const dots = document.querySelectorAll('.question-number-dot');
+        const randomIndex = Math.floor(Math.random() * this.state.bgColors.length);
+        const bgColor = this.state.bgColors[randomIndex];
+        const textColor = this.state.textColors[randomIndex];
+
+        // css variables
+        body.style.setProperty('--prev-bg-color', this.state.prevBgColor);
+        body.style.setProperty('--prev-text-color', this.state.prevTextColor);
+        body.style.setProperty('--bg-color', bgColor);
+        body.style.setProperty('--text-color', textColor);
+
+        body.classList.remove('first-answer');
+        if(body.classList.contains('following-answer-1')){
+            body.classList.remove('following-answer-1');
+            body.classList.add('following-answer-2');
+        }else if(body.classList.contains('following-answer-2')){
+            body.classList.remove('following-answer-2');
+            body.classList.add('following-answer-1');
         }else{
-            if(this.state.prevAnswer != answerValue){
-                if(answerValue === 0){
-                    body.classList.remove('first-negative-answer');
-                    body.classList.remove('first-positive-answer');
-                    body.classList.remove('positive-answer');
-                    body.classList.add('negative-answer');
-                }else {
-                    body.classList.remove('first-negative-answer');
-                    body.classList.remove('first-positive-answer');
-                    body.classList.remove('negative-answer');
-                    body.classList.add('positive-answer');
-                }
-            }
+            body.classList.add('following-answer-1');
         }
+        body.style.backgroundColor = bgColor;
+        dots.forEach(dot => {
+            dot.style.backgroundColor = textColor;
+        });
         
-        const prevAnswer = answerValue;
+        const prevBgColor = bgColor;
+        const prevTextColor = textColor;
         let answerTargetCategoryMap = this.state.targetCategories;
         answerTargetCategoryMap[this.props.questions[answerIndex].targetCategory] = answerValue;
         this.setState({ 
             targetCategories: answerTargetCategoryMap,
             pageNum: this.state.pageNum != this.props.questions.length ? this.state.pageNum + 1 : this.state.pageNum,
-            prevAnswer: prevAnswer
+            prevBgColor: prevBgColor,
+            prevTextColor: prevTextColor
+        });
+    }
+
+    handleMouseEnter(){
+        const body = document.querySelector('body');
+        const dots = document.querySelectorAll('.question-number-dot');
+        const randomIndex = Math.floor(Math.random() * this.state.bgColors.length);
+        const bgColor = this.state.bgColors[randomIndex];
+        const textColor = this.state.textColors[randomIndex];
+
+        // css variables
+        body.style.setProperty('--prev-bg-color', this.state.prevBgColor);
+        body.style.setProperty('--prev-text-color', this.state.prevTextColor);
+        body.style.setProperty('--bg-color', bgColor);
+        body.style.setProperty('--text-color', textColor);
+
+        body.classList.remove('first-answer');
+        if(body.classList.contains('following-answer-1')){
+            body.classList.remove('following-answer-1');
+            body.classList.add('following-answer-2');
+        }else if(body.classList.contains('following-answer-2')){
+            body.classList.remove('following-answer-2');
+            body.classList.add('following-answer-1');
+        }else{
+            body.classList.add('following-answer-1');
+        }
+        body.style.backgroundColor = bgColor;
+        dots.forEach(dot => {
+            dot.style.backgroundColor = textColor;
+        });
+        
+        const prevBgColor = bgColor;
+        const prevTextColor = textColor;
+        this.setState({
+            prevBgColor: prevBgColor,
+            prevTextColor: prevTextColor
+        });
+    }
+
+    handleMouseLeave(){
+        const body = document.querySelector('body');
+        const dots = document.querySelectorAll('.question-number-dot');
+        const bgColor = '#131316';
+        const textColor = 'white';
+
+        // css variables
+        body.style.setProperty('--prev-bg-color', this.state.prevBgColor);
+        body.style.setProperty('--prev-text-color', this.state.prevTextColor);
+        body.style.setProperty('--bg-color', bgColor);
+        body.style.setProperty('--text-color', textColor);
+
+        body.classList.remove('first-answer');
+        if(body.classList.contains('following-answer-1')){
+            body.classList.remove('following-answer-1');
+            body.classList.add('following-answer-2');
+        }else if(body.classList.contains('following-answer-2')){
+            body.classList.remove('following-answer-2');
+            body.classList.add('following-answer-1');
+        }else{
+            body.classList.add('following-answer-1');
+        }
+        body.style.backgroundColor = bgColor;
+        dots.forEach(dot => {
+            dot.style.backgroundColor = textColor;
+        });
+        
+        const prevBgColor = bgColor;
+        const prevTextColor = textColor;
+        this.setState({
+            prevBgColor: prevBgColor,
+            prevTextColor: prevTextColor
         });
     }
 
@@ -73,19 +155,18 @@ class QuestionsForm extends React.Component {
         const { pageNum } = this.state;
         const { questions } = this.props;
         const question = questions[pageNum - 1];
-        const submitButton = <button onClick={this.generatePlaylist}>Create Playlist</button>;
-        return (
-            <div>
-                <div className="question-container">
+        const submitButton = <button className="questions-submit-button" onClick={this.generatePlaylist}>Create Playlist</button>;
+        const questionsContent = (
                     <div className="question-content">
                         <div className="question-text">{question.question}</div>
                         <div className="answers-container">
                             {question.answerOptions.map((answerOption, i) => {
                                 return (
-                                    <div className="answer-option-container" key={`answer-${i}`}>
+                                    <div className="answer-option-container" onClick={() => this.handleInput(pageNum - 1, answerOption.answerValue)} key={`answer-${i}`}>
                                         <div 
                                             className="answer-option" 
-                                            onClick={() => this.handleInput(pageNum - 1, answerOption.answerValue)}
+                                            onMouseEnter={this.handleMouseEnter}
+                                            onMouseLeave={this.handleMouseLeave}
                                         >
                                             {answerOption.answerText}
                                         </div>
@@ -93,19 +174,20 @@ class QuestionsForm extends React.Component {
                                 );
                             })}
                         </div>
-                        {
-                            (pageNum === this.props.questions.length) ? submitButton : (
-                                <div className="page-num-indicator">
-                                    {questions.map((question, i) => {
-                                        const largeDot = pageNum === i + 1 ? 'large-dot' : '';
-                                        return (
-                                            <div className={`question-number-dot ${largeDot}`}></div>
-                                        );
-                                    })}
-                                </div>
-                            )
-                        }
+                        <div className="page-num-indicator">
+                            {questions.map((question, i) => {
+                                const largeDot = pageNum === i + 1 ? 'large-dot' : '';
+                                return (
+                                    <div className={`question-number-dot ${largeDot}`}></div>
+                                );
+                            })}
+                        </div>
                     </div>
+                );
+        return (
+            <div>
+                <div className="question-container">
+                    {pageNum === this.props.questions.length ? submitButton : questionsContent}
                 </div>
             </div>
         );
