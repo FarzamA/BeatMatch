ba198b08100729d83610e1d8399b66256d0d1717
Zeki <zckural@gmail.com>
2021-11-22 01:56:16 -0500

    remove console logs

--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -5,7 +5,6 @@ const _nullState = {};
 
 const usersReducer = (state = _nullState, action) => {
     Object.freeze(state);
-    debugger
     switch(action.type) {
         case RECEIVE_USER:
             let userWithoutFollowers = _.omit(action.user, ['followers', 'followed']);
