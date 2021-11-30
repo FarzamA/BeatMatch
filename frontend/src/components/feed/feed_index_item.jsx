850393f19c535d0a084b31c2c7415235d46860e6
Elliot Wilson <87868033+elliot-wilson@users.noreply.github.com>
2021-11-30 17:19:52 -0500

    add basic date display

--- frontend/src/components/feed/feed_index_item.jsx
+++ frontend/src/components/feed/feed_index_item.jsx
@@ -4,12 +4,15 @@ const FeedIndexItem = (props) => {
 
     const { feedPlaylist } = props;
 
+    const date = new Date(feedPlaylist.creation_date);
+
     return (
         <li className="feed-index-item">
+            <p>{feedPlaylist.creator_name} created this playlist on {date.toLocaleDateString()} at {date.toLocaleTimeString()}</p>
             <iframe
                 src={feedPlaylist.spotify_embed_link}
-                width="400"
-                height="380"
+                width="500"
+                height="680"
                 frameBorder="0"
                 allowtransparency="true"
                 allow="encrypted-media"
