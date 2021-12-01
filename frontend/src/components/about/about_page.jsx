6d8ec3a13c14a9081c07bc233f23398a156ca6b5
Elliot Wilson <87868033+elliot-wilson@users.noreply.github.com>
2021-11-30 21:06:48 -0500

    fixed angellist link for Elliot

--- frontend/src/components/about/about_page.jsx
+++ frontend/src/components/about/about_page.jsx
@@ -49,7 +49,7 @@ const AboutPage = () => {
                         <div className="team-member-links">
                             <a className="team-member-social-link" target="_blank" href="https://www.linkedin.com/in/elliot-wilson-4897a81b9/"><i className="fab fa-linkedin-in"></i></a>
                             <a className="team-member-social-link" target="_blank" href="https://github.com/elliot-wilson"><i className="fab fa-github"></i></a>
-                            <a className="team-member-social-link" target="_blank" href="https://angel.co"><i className="fab fa-angellist"></i></a>
+                            <a className="team-member-social-link" target="_blank" href="https://angel.co/u/elliot-wilson-dev"><i className="fab fa-angellist"></i></a>
                             <a className="team-member-social-link" target="_blank" href="https://www.linkedin.com/in/elliot-wilson-4897a81b9/"><i className="fas fa-user-tie"></i></a>
                         </div>
                     </div>
